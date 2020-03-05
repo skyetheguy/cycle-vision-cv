7b213edeaeba8ffd099023615787386e7ed846f0
skyetheguy <mhosue@uwaterloo.ca>
2020-03-05 10:50:11 -0500

    flashing right turn sig works

--- bin/gpio_demo.py
+++ bin/gpio_demo.py
@@ -4,7 +4,6 @@ import time
 import RPi.GPIO as GPIO
 
 # state_1 = False
-# state_changed_event = threading.Event()
 # led_pin = 12
 # but_pin = 18
 
@@ -20,11 +19,11 @@ left_turn_lights = [left_turn_front_light, left_turn_rear_light]
 left_turn_lights_state = False
 left_turn_lights_button = None
 
-right_turn_front_light = None
-right_turn_rear_light = None
+right_turn_front_light = 11
+right_turn_rear_light = 16
 right_turn_lights = [right_turn_front_light, right_turn_rear_light]
 right_turn_lights_state = False
-right_turn_lights_button = None
+right_turn_lights_button = 37
 
 left_blind_spot_light = None
 left_blind_spot_light_state = False
@@ -35,8 +34,11 @@ blind_spot_lights = [left_blind_spot_light, right_blind_spot_light]
 # light_control_pins = night_lights + left_turn_lights + right_turn_lights + blind_spot_lights
 # button_pins = [night_lights_button, left_turn_lights_button, right_turn_lights_button]
 
-light_control_pins = night_lights
-button_pins = [night_lights_button]
+light_control_pins = night_lights + right_turn_lights
+button_pins = [night_lights_button, right_turn_lights_button]
+
+# Events
+turn_sig_event = threading.Event()
 
 # Toggle front and rear light callback
 def toggle_night_lights(channel):
@@ -57,23 +59,19 @@ def toggle_night_lights(channel):
 # Toggle right front and rear turn lights callback
 def toggle_right_turn_lights(channel):
     global right_turn_lights_state
+    global turn_sig_event
 
     if right_turn_lights_state:
         logging.info("Right turn sig OFF")
 
-        right_turn_lights_state ^= True
+        right_turn_lights_state = False
     else:
         logging.info("Right turn sig ON")
 
-        right_turn_lights_state ^= True
-        while (right_turn_lights_state):
-            # Flash lights
-            for pin in right_turn_lights:
-                GPIO.output(pin, GPIO.HIGH)
-            time.sleep(1)
-            for pin in right_turn_lights:
-                GPIO.output(pin, GPIO.LOW)
-            time.sleep(1)
+        right_turn_lights_state = True
+        turn_sig_event.set()
+
+        
 
 
 # Toggle left front and rear turn lights callback
@@ -97,7 +95,29 @@ def toggle_left_turn_lights(channel):
                 GPIO.output(pin, GPIO.LOW)
             time.sleep(1)
 
+# Test thread function
+def flash_turn_sigs():
+    global right_turn_lights_state
+    global turn_sig_event
+
+    while True:
+        turn_sig_event.wait()
+        turn_sig_event.clear()
+
+        while (right_turn_lights_state):
+            # Flash lights
+            # logging.info("  FLASH")
+            # time.sleep(1)
+            for pin in right_turn_lights:
+                GPIO.output(pin, GPIO.HIGH)
+            time.sleep(0.5)
+            for pin in right_turn_lights:
+                GPIO.output(pin, GPIO.LOW)
+            time.sleep(0.5)
+
 def init():
+    global turn_sig_event
+
     # Pin Setup
     # ==========================================================
     # BOARD pin-numbering scheme
@@ -116,25 +136,21 @@ def init():
         GPIO.output(pin, GPIO.LOW)
 
     GPIO.add_event_detect(night_lights_button, GPIO.FALLING, callback=toggle_night_lights, bouncetime=100)
-    # GPIO.add_event_detect(left_turn_lights_button, GPIO.FALLING, callback=toggle_right_turn_lights, bouncetime=100)
-    # GPIO.add_event_detect(right_turn_lights_button, GPIO.FALLING, callback=toggle_left_turn_lights, bouncetime=100)
+    GPIO.add_event_detect(right_turn_lights_button, GPIO.FALLING, callback=toggle_right_turn_lights, bouncetime=100)
+    # GPIO.add_event_detect(left_turn_lights_button, GPIO.FALLING, callback=toggle_left_turn_lights, bouncetime=100)
     # ===========================================
 
+    # Settup threads
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    turn_sig_event.clear()
 
-# # Test thread function
-# def thread_function(name):
-#     # logging.info("Thread %s: starting", name)
-
-#     while(True):
-#         state_changed_event.wait()
-#         state_changed_event.clear()
-
-#         if(state_1):
-#             GPIO.output(led_pin, GPIO.HIGH)
-#         else:
-#             GPIO.output(led_pin, GPIO.LOW)
+    turn_sig_thread = threading.Thread(target=flash_turn_sigs, args=())
+    turn_sig_thread.start()
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
-#     # logging.info("Thread %s: finishing", name)
+
+
+
 
 # # Test interrupt callback
 # def interrupt_callback(channel):

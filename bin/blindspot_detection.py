211e2f0097b6fe56bf057722d5ce832ac8b4b692
Zaki Ahmed <mz3ahmed@uwaterloo.ca>
2020-03-09 08:53:36 -0400

    Test on and off

--- bin/blindspot_detection.py
+++ bin/blindspot_detection.py
@@ -8,15 +8,16 @@ camera = jetson.utils.gstCamera(640, 360, "0", "/home/c-vis/cycle-vision-cv/vide
 display = jetson.utils.glDisplay()
 
 gpio.init()
-gpio.update_blind_spot("left", True)
+gpio.update_blind_spot("right", True)
 
 while display.IsOpen():
     img, width, height = camera.CaptureRGBA()
     detections = net.Detect(img, width, height)
     for detection in detections:
+		gpio.update_blind_spot("right", True)
     	print("Saw an object:    ")
     	print( detection.ClassID, detection.Area, detection.Center )
     	print("\n")
-
+    gpio.update_blind_spot("right", False)
     display.RenderOnce(img, width, height)
     display.SetTitle("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))

1315f2e471ecdbc3b4ffc326a593e172d53030a3
NANO <mz3ahmed@uwaterloo.ca>
2020-03-09 08:24:53 -0400

    Update scripts

--- bin/blindspot_detection.py
+++ bin/blindspot_detection.py
@@ -1,10 +1,13 @@
 import jetson.inference
 import jetson.utils
+import gpio_demo as gpio
 
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.2225)
-#camera = jetson.utils.gstCamera(640, 360, "0", "/home/c-vis/cycle-vision-cv/videos/test.yuv")
-camera = jetson.utils.gstCamera(1280, 720)
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.25)
+camera = jetson.utils.gstCamera(640, 360, "0", "/home/c-vis/cycle-vision-cv/videos/test.yuv")
+#camera = jetson.utils.gstCamera(1280, 720)
 display = jetson.utils.glDisplay()
+
+gpio.init()
 
 while display.IsOpen():
     img, width, height = camera.CaptureRGBA()
--- bin/run_nn.py
+++ bin/run_nn.py
@@ -1,9 +1,9 @@
 import jetson.inference
 import jetson.utils
 
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.2225)
-#camera = jetson.utils.gstCamera(640, 360, "0", "/home/c-vis/cycle-vision-cv/videos/test.yuv", 30)
-camera = jetson.utils.gstCamera(1280, 720)
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.35)
+camera = jetson.utils.gstCamera(1280, 720, "0", "/home/c-vis/cycle-vision-cv/videos/delete2.yuv", 30)
+#camera = jetson.utils.gstCamera(1280, 720, "/dev/video1")
 display = jetson.utils.glDisplay()
 
 while display.IsOpen():
